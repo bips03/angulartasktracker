10802d70dba576317631ca28efc08acffab81b5d
bips03 <biplabps2@gmail.com>
2022-03-29 15:10:36 +0530

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyBuwSOpEUSH81g4zmcrD-atK-dRb31PtGM',
+    authDomain: 'angular-tasktracker.firebaseapp.com',
+    projectId: 'angular-tasktracker',
+    storageBucket: 'angular-tasktracker.appspot.com',
+    messagingSenderId: '229226861756',
+    appId: '1:229226861756:web:84816150f7cdd80896ecb9',
+  }
 };
